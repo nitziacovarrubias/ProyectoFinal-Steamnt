ca5f6c9f2be185c3d6e020be21c61099caddf524
AsHOPStore <palatron33@gmail.com>
2026-05-21 21:02:05 -0700

    Feat: Added user register endpoint

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Steamnt.Api.Data;
+using Steamnt.Api.Dtos.Auth;
+using Steamnt.Api.Models;
+
+namespace Steamnt.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+    public class AuthController: ControllerBase
+    {
+    private readonly AppDbContext contexto;
+
+    public AuthController(AppDbContext contexto)
+    {
+        this.contexto = contexto;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterDTO dto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var emailExists = await contexto.Users
+        .AnyAsync(u => u.Email == dto.Email);
+
+        if (emailExists)
+        {
+            return BadRequest("Ya esta este correo registrado");
+        }
+
+        var user = new User
+        {
+            Name = dto.Name,
+            Email = dto.Email,
+            PasswordHash = dto.Password,
+            Role = "User"
+        };
+
+        await contexto.Users.AddAsync(user);
+
+        await contexto.SaveChangesAsync();
+
+        return Ok("Usuario Creado Correctamente!");
+
+    }
+}
---
+++ Dtos/Auth/RegisterDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Steamnt.Api.Dtos.Auth
+{
+    public class RegisterDTO
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
