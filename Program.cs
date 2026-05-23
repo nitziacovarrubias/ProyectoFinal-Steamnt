c5e23c978cf2be76c374343c0feb5daf8f4d221e
Nitzia Covarrubias <141579033+nitziacovarrubias@users.noreply.github.com>
2026-05-23 13:45:49 -0700

    feat: configurar CORS para permitir solicitudes desde vite frontend

--- Program.cs
+++ Program.cs
@@ -15,7 +15,18 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IDeveloperService, DeveloperService>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
+app.UseCors("AllowFrontend");
 
 if (app.Environment.IsDevelopment())
 {
